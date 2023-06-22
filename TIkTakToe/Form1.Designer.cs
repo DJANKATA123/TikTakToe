062b9852a6f2200189ef7936c64690adf0543775
DJANKATA123 <blebleid211@gmail.com>
2023-06-22 19:20:16 +0300

    Added game TikTakToe

---
+++ TIkTakToe/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace TIkTakToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.t1 = new System.Windows.Forms.Label();
+            this.t5 = new System.Windows.Forms.Label();
+            this.t4 = new System.Windows.Forms.Label();
+            this.t3 = new System.Windows.Forms.Label();
+            this.t2 = new System.Windows.Forms.Label();
+            this.t9 = new System.Windows.Forms.Label();
+            this.t7 = new System.Windows.Forms.Label();
+            this.t8 = new System.Windows.Forms.Label();
+            this.t6 = new System.Windows.Forms.Label();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // t1
+            // 
+            this.t1.BackColor = System.Drawing.Color.White;
+            this.t1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.t1.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.t1.Location = new System.Drawing.Point(153, 65);
+            this.t1.Name = "t1";
+            this.t1.Size = new System.Drawing.Size(96, 85);
+            this.t1.TabIndex = 0;
+            this.t1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.t1.Click += new System.EventHandler(this.t1_Click);
+            // 
+            // t5
+            // 
+            this.t5.BackColor = System.Drawing.Color.White;
+            this.t5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.t5.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.t5.Location = new System.Drawing.Point(291, 183);
+            this.t5.Name = "t5";
+            this.t5.Size = new System.Drawing.Size(96, 85);
+            this.t5.TabIndex = 1;
+            this.t5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.t5.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // t4
+            // 
+            this.t4.BackColor = System.Drawing.Color.White;
+            this.t4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.t4.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.t4.Location = new System.Drawing.Point(153, 183);
+            this.t4.Name = "t4";
+            this.t4.Size = new System.Drawing.Size(96, 85);
+            this.t4.TabIndex = 2;
+            this.t4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.t4.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // t3
+            // 
+            this.t3.BackColor = System.Drawing.Color.White;
+            this.t3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.t3.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.t3.Location = new System.Drawing.Point(427, 65);
+            this.t3.Name = "t3";
+            this.t3.Size = new System.Drawing.Size(96, 85);
+            this.t3.TabIndex = 3;
+            this.t3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.t3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // t2
+            // 
+            this.t2.BackColor = System.Drawing.Color.White;
+            this.t2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.t2.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.t2.Location = new System.Drawing.Point(291, 65);
+            this.t2.Name = "t2";
+            this.t2.Size = new System.Drawing.Size(96, 85);
+            this.t2.TabIndex = 4;
+            this.t2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.t2.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // t9
+            // 
+            this.t9.BackColor = System.Drawing.Color.White;
+            this.t9.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.t9.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.t9.Location = new System.Drawing.Point(427, 300);
+            this.t9.Name = "t9";
+            this.t9.Size = new System.Drawing.Size(96, 85);
+            this.t9.TabIndex = 5;
+            this.t9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.t9.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // t7
+            // 
+            this.t7.BackColor = System.Drawing.Color.White;
+            this.t7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.t7.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.t7.Location = new System.Drawing.Point(153, 300);
+            this.t7.Name = "t7";
+            this.t7.Size = new System.Drawing.Size(96, 85);
+            this.t7.TabIndex = 6;
+            this.t7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.t7.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // t8
+            // 
+            this.t8.BackColor = System.Drawing.Color.White;
+            this.t8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.t8.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.t8.Location = new System.Drawing.Point(291, 300);
+            this.t8.Name = "t8";
+            this.t8.Size = new System.Drawing.Size(96, 85);
+            this.t8.TabIndex = 7;
+            this.t8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.t8.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // t6
+            // 
+            this.t6.BackColor = System.Drawing.Color.White;
+            this.t6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.t6.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.t6.Location = new System.Drawing.Point(427, 183);
+            this.t6.Name = "t6";
+            this.t6.Size = new System.Drawing.Size(96, 85);
+            this.t6.TabIndex = 8;
+            this.t6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.t6.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnNewGame.Location = new System.Drawing.Point(274, 412);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(131, 58);
+            this.btnNewGame.TabIndex = 9;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Visible = false;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 482);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.t6);
+            this.Controls.Add(this.t8);
+            this.Controls.Add(this.t7);
+            this.Controls.Add(this.t9);
+            this.Controls.Add(this.t2);
+            this.Controls.Add(this.t3);
+            this.Controls.Add(this.t4);
+            this.Controls.Add(this.t5);
+            this.Controls.Add(this.t1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label t1;
+        private System.Windows.Forms.Label t5;
+        private System.Windows.Forms.Label t4;
+        private System.Windows.Forms.Label t3;
+        private System.Windows.Forms.Label t2;
+        private System.Windows.Forms.Label t9;
+        private System.Windows.Forms.Label t7;
+        private System.Windows.Forms.Label t8;
+        private System.Windows.Forms.Label t6;
+        private System.Windows.Forms.Button btnNewGame;
+    }
+}
+
---
+++ TIkTakToe/Form1.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TIkTakToe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        public bool XOcheck = true;
+        public int drawcount = 0;
+        public void CheckWin()
+        {
+            if (t1.Text == "X" && t2.Text == "X" && t3.Text == "X")
+            {
+                t1.BackColor = Color.Green;
+                t2.BackColor = Color.Green;
+                t3.BackColor = Color.Green;
+                MessageBox.Show("X wins!");
+                btnNewGame.Visible = true;
+            }
+            else if (t4.Text == "X" && t5.Text == "X" && t6.Text == "X")
+            {
+                t4.BackColor = Color.Green;
+                t5.BackColor = Color.Green;
+                t6.BackColor = Color.Green;
+                MessageBox.Show("X wins!");
+                btnNewGame.Visible = true;
+            }
+            else if (t7.Text == "X" && t8.Text == "X" && t9.Text == "X")
+            {
+                t7.BackColor = Color.Green;
+                t8.BackColor = Color.Green;
+                t9.BackColor = Color.Green;
+                MessageBox.Show("X wins!");
+                btnNewGame.Visible = true;
+            }
+            else if (t1.Text == "X" && t4.Text == "X" && t7.Text == "X")
+            {
+                t1.BackColor = Color.Green;
+                t4.BackColor = Color.Green;
+                t7.BackColor = Color.Green;
+                MessageBox.Show("X wins!");
+                btnNewGame.Visible = true;
+            }
+            else if (t2.Text == "X" && t5.Text == "X" && t8.Text == "X")
+            {
+                t2.BackColor = Color.Green;
+                t5.BackColor = Color.Green;
+                t8.BackColor = Color.Green;
+                MessageBox.Show("X wins!");
+                btnNewGame.Visible = true;
+            }
+            else if (t3.Text == "X" && t6.Text == "X" && t9.Text == "X")
+            {
+                t3.BackColor = Color.Green;
+                t6.BackColor = Color.Green;
+                t9.BackColor = Color.Green;
+                MessageBox.Show("X wins!");
+                btnNewGame.Visible = true;
+            }
+            else if (t1.Text == "X" && t5.Text == "X" && t9.Text == "X")
+            {
+                t1.BackColor = Color.Green;
+                t5.BackColor = Color.Green;
+                t9.BackColor = Color.Green;
+                MessageBox.Show("X wins!");
+                btnNewGame.Visible = true;
+            }
+           else  if (t3.Text == "X" && t5.Text == "X" && t7.Text == "X")
+            {
+                t3.BackColor = Color.Green;
+                t5.BackColor = Color.Green;
+                t7.BackColor = Color.Green;
+                MessageBox.Show("X wins!");
+                btnNewGame.Visible = true;
+            }
+            else if (t1.Text == "O" && t2.Text == "O" && t3.Text == "O")
+            {
+                t1.BackColor = Color.Green;
+                t2.BackColor = Color.Green;
+                t3.BackColor = Color.Green;
+                MessageBox.Show("O wins!");
+                btnNewGame.Visible = true;
+            }
+           else  if (t4.Text == "O" && t5.Text == "O" && t6.Text == "O")
+            {
+                t4.BackColor = Color.Green;
+                t5.BackColor = Color.Green;
+                t6.BackColor = Color.Green;
+                MessageBox.Show("O wins!");
+                btnNewGame.Visible = true;
+            }
+          else   if (t7.Text == "O" && t8.Text == "O" && t9.Text == "O")
+            {
+                t7.BackColor = Color.Green;
+                t8.BackColor = Color.Green;
+                t9.BackColor = Color.Green;
+                MessageBox.Show("X wins!");
+                btnNewGame.Visible = true;
+            }
+           else  if (t1.Text == "O" && t4.Text == "O" && t7.Text == "O")
+            {
+                t1.BackColor = Color.Green;
+                t4.BackColor = Color.Green;
+                t7.BackColor = Color.Green;
+                MessageBox.Show("O wins!");
+                btnNewGame.Visible = true;
+            }
+          else   if (t2.Text == "O" && t5.Text == "O" && t8.Text == "O")
+            {
+                t2.BackColor = Color.Green;
+                t5.BackColor = Color.Green;
+                t8.BackColor = Color.Green;
+                MessageBox.Show("O wins!");
+                btnNewGame.Visible = true;
+            }
+           else  if (t3.Text == "O" && t6.Text == "O" && t9.Text == "O")
+            {
+                t3.BackColor = Color.Green;
+                t6.BackColor = Color.Green;
+                t9.BackColor = Color.Green;
+                MessageBox.Show("O wins!");
+                btnNewGame.Visible = true;
+            }
+           else  if (t1.Text == "O" && t5.Text == "O" && t9.Text == "O")
+            {
+                t1.BackColor = Color.Green;
+                t5.BackColor = Color.Green;
+                t9.BackColor = Color.Green;
+                MessageBox.Show("X wins!");
+                btnNewGame.Visible = true;
+            }
+            else if (t3.Text == "O" && t5.Text == "O" && t7.Text == "O")
+            {
+                t3.BackColor = Color.Green;
+                t5.BackColor = Color.Green;
+                t7.BackColor = Color.Green;
+                MessageBox.Show("O wins!");
+                btnNewGame.Visible = true;
+            }
+            else if (drawcount == 9)
+            {
+                MessageBox.Show("Draw!");
+                btnNewGame.Visible = true;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            if (XOcheck == true)
+            {
+                t2.Text = "X";
+            }
+            if (XOcheck == false)
+            {
+                t2.Text = "O";
+            }
+            XOcheck = !XOcheck;
+            t2.Enabled = false;
+            drawcount++;
+            CheckWin();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            if (XOcheck == true)
+            {
+                t4.Text = "X";
+            }
+            if (XOcheck == false)
+            {
+                t4.Text = "O";
+            }
+            XOcheck = !XOcheck;
+            t4.Enabled = false;
+            drawcount++;
+            CheckWin();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            if (XOcheck == true)
+            {
+                t7.Text = "X";
+            }
+            if (XOcheck == false)
+            {
+                t7.Text = "O";
+            }
+            XOcheck = !XOcheck;
+            t7.Enabled = false;
+            drawcount++;
+            CheckWin();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            if (XOcheck == true)
+            {
+                t8.Text = "X";
+            }
+            if (XOcheck == false)
+            {
+                t8.Text = "O";
+            }
+            XOcheck = !XOcheck;
+            t8.Enabled = false;
+            drawcount++;
+            CheckWin();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            if (XOcheck == true)
+            {
+                t5.Text = "X";
+            }
+            if (XOcheck == false)
+            {
+                t5.Text = "O";
+            }
+            XOcheck = !XOcheck;
+            t5.Enabled = false;
+            drawcount++;
+            CheckWin();
+        }
+
+        private void t1_Click(object sender, EventArgs e)
+        {
+            if (XOcheck == true)
+            {
+                t1.Text = "X";
+            }
+            if (XOcheck == false)
+            {
+                t1.Text = "O";
+            }
+            t1.Enabled = false;
+            XOcheck = !XOcheck;
+            drawcount++;
+            CheckWin();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            if (XOcheck == true)
+            {
+                t3.Text = "X";
+            }
+            if (XOcheck == false)
+            {
+                t3.Text = "O";
+            }
+            XOcheck = !XOcheck;
+            t3.Enabled = false;
+            drawcount++;
+            CheckWin();
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            if (XOcheck == true)
+            {
+                t6.Text = "X";
+            }
+            if (XOcheck == false)
+            {
+                t6.Text = "O";
+            }
+            XOcheck = !XOcheck;
+            t6.Enabled = false;
+            drawcount++;
+            CheckWin();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            if (XOcheck == true)
+            {
+                t9.Text = "X";
+            }
+            if (XOcheck == false)
+            {
+                t9.Text = "O";
+            }
+            XOcheck = !XOcheck;
+            t9.Enabled = false;
+            drawcount++;
+            CheckWin();
+        }
+
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            t1.Text = "";
+            t1.BackColor = Color.White;
+            t2.Text = "";
+            t2.BackColor = Color.White;
+            t3.Text = "";
+            t3.BackColor = Color.White;
+            t4.Text = "";
+            t4.BackColor = Color.White;
+            t5.Text = "";
+            t5.BackColor = Color.White;
+            t6.Text = "";
+            t6.BackColor = Color.White;
+            t7.Text = "";
+            t7.BackColor = Color.White;
+            t8.Text = "";
+            t8.BackColor = Color.White;
+            t9.Text = "";
+            t9.BackColor = Color.White;
+            btnNewGame.Visible = false;
+            t1.Enabled = true;
+            t2.Enabled = true;
+            t3.Enabled = true;
+            t4.Enabled = true;
+            t5.Enabled = true;
+            t6.Enabled = true;
+            t7.Enabled = true;
+            t8.Enabled = true;
+            t9.Enabled = true;
+            drawcount = 0;
+        }
+    }
+}
